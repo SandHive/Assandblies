60d0bb5de57d5cd0dbbde2fc4f18fffdf0ab25a3
Sandmaker <sandhive@gmail.com>
2013-06-23 21:50:07 +0200

    "SandPanelPrototype" project added

---
+++ Prototypes/SandPanelPrototype/SandPanelPrototype/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Sand.Controls;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+//-----------------------------------------------------------------------------
+namespace Prototype
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //---------------------------------------------------------------------
+        #region Constructors
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+        //---------------------------------------------------------------------
+        #region Event Handling
+      
+        private void Window_Loaded( object sender, RoutedEventArgs e )
+        {
+            _sandPanelWidgetGrid.AddItem( new SandPanelWidget() { Content = new Image() { Source = new BitmapImage( new Uri( @"pack://application:,,,/SandPanelPrototype;component/Images/convert_icon256.png" ) ) } } );
+            _sandPanelWidgetGrid.AddItem( new SandPanelWidget() { Content = new Image() { Source = new BitmapImage( new Uri( @"pack://application:,,,/SandPanelPrototype;component/Images/convert_icon256.png" ) ) } } );
+            _sandPanelWidgetGrid.AddItem( new SandPanelWidget() { Content = new Image() { Source = new BitmapImage( new Uri( @"pack://application:,,,/SandPanelPrototype;component/Images/convert_icon256.png" ) ) } } );
+        }
+        
+        #endregion Event Handling
+        //---------------------------------------------------------------------
+    }
+}
+//-----------------------------------------------------------------------------
