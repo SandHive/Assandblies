fc8bf5c190e418f9c0df25ce94f0a66de9f9ca2c
Sandman <sandhive@gmail.com>
2016-08-12 17:59:04 +0200

    Some Fhem functionality added

---
+++ Fhem/Basics/FhemClient.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+//-----------------------------------------------------------------------------
+namespace Sand.Fhem.Basics
+{
+    public class FhemClient : IDisposable
+    {
+        //---------------------------------------------------------------------
+        #region Fields
+
+        private TcpClient  m_tcpClient;
+
+        //-- Fields
+        #endregion
+        //---------------------------------------------------------------------
+        #region Properties
+
+        public int ReadBufferSize { get; set; } = 1024;
+
+        public int ReadTimeoutInMs { get; set; } = 1000;
+
+        //-- Properties
+        #endregion
+        //---------------------------------------------------------------------
+        #region Constructors
+
+        public FhemClient( string a_hostName, int a_port )
+        {
+            m_tcpClient = new TcpClient( a_hostName, a_port );
+        }
+
+        //-- Constructors
+        #endregion
+        //---------------------------------------------------------------------
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            m_tcpClient?.Close();
+        }
+
+        //-- IDisposable Members
+        #endregion
+        //---------------------------------------------------------------------
+        #region Methods
+            
+        [DebuggerStepThrough]
+        public string GetApplicationTime()
+        {
+            return this.SendCommand( "apptime" );
+        }
+
+        [DebuggerStepThrough]
+        public string GetXmlList()
+        {
+            return this.SendCommand( "xmllist" );
+        }
+
+        public string SendCommand( string a_commandString )
+        {
+            //-- Each FHEM command must end with "\r\n"
+            if( !a_commandString.EndsWith( "\r\n" ) )
+            {
+                a_commandString = a_commandString + "\r\n";
+            }
+
+            using( var networkStream = m_tcpClient.GetStream() )
+            {
+                var writeBuffer = Encoding.ASCII.GetBytes( a_commandString );
+
+                networkStream.Write( writeBuffer, 0, writeBuffer.Length );
+                networkStream.Flush();
+
+                var readingDateTime = DateTime.Now;
+
+                var readBuffer = new byte[this.ReadBufferSize];
+
+                StringBuilder stringBuilder = null;
+
+                do
+                {
+                    if( networkStream.DataAvailable )
+                    {
+                        var readBytesCount = networkStream.Read( readBuffer, 0, this.ReadBufferSize );
+
+                        if( readBytesCount == this.ReadBufferSize )
+                        {
+                            if( stringBuilder == null )
+                            {
+                                stringBuilder = new StringBuilder();
+                            }
+
+                            string readBufferAsString = Encoding.ASCII.GetString( readBuffer );
+
+                            stringBuilder.Append( readBufferAsString );
+
+                            readingDateTime = DateTime.Now;
+                        }
+                        else
+                        {
+                            string readBufferAsString = Encoding.ASCII.GetString( readBuffer );
+
+                            if( stringBuilder != null )
+                            {
+                                stringBuilder.Append( readBufferAsString );
+
+                                return stringBuilder.ToString();
+                            }
+
+                            return readBufferAsString;
+                        }                                                
+                    }
+                }
+                while( ( (TimeSpan) ( DateTime.Now - readingDateTime ) ).TotalMilliseconds <= this.ReadTimeoutInMs );
+
+                return String.Empty;
+            }
+        }
+
+        //-- Methods
+        #endregion
+        //---------------------------------------------------------------------
+    }
+}
+//-----------------------------------------------------------------------------
--- Fhem/Prism/FhemModule/ViewModels/FhemExplorerViewModel.cs
+++ Fhem/Prism/FhemModule/ViewModels/FhemExplorerViewModel.cs
@@ -1,13 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Sand.Fhem.Basics;
+//-----------------------------------------------------------------------------
 namespace Sand.Fhem.Prism.FhemModule.ViewModels
 {
-    public class FhemExplorerViewModel
+    public class FhemExplorerViewModel : BindableBase
     {
-        public string TestText { get { return "Shabubu"; } }
-    } 
+        //---------------------------------------------------------------------
+        #region Fields
+
+        private FhemClient  m_fhemClient;
+
+        private string  m_fhemResponse;
+
+        //-- Fields
+        #endregion
+        //---------------------------------------------------------------------
+        #region Properties
+
+        public DelegateCommand ConnectCommand { get; private set; }
+
+        public string FhemCommand { get; set; }
+
+        public string FhemResponse
+        {
+            get { return m_fhemResponse; }
+            set
+            {
+                if( value == m_fhemResponse ) { return; }
+
+                m_fhemResponse = value;
+
+                this.OnPropertyChanged();
+            }
+        }
+
+        public string FhemServerIP { get; set; } = "192.168.178.50";
+
+        public string FhemServerPort { get; set; } = "7072";
+
+        public DelegateCommand SendCommand { get; private set; }
+
+        //-- Properties
+        #endregion
+        //---------------------------------------------------------------------
+        #region Constructors
+
+        public FhemExplorerViewModel()
+        {
+            //-- Initialize commands
+            this.ConnectCommand = new DelegateCommand( () => this.ConnectCommandAction()  );
+            this.SendCommand = new DelegateCommand( () => this.SendCommandAction() );
+        }
+
+        //-- Constructors
+        #endregion
+        //---------------------------------------------------------------------
+        #region Methods
+
+        private void ConnectCommandAction()
+        {
+            m_fhemClient = new FhemClient( this.FhemServerIP, int.Parse( this.FhemServerPort ) );
+        }
+
+        private void SendCommandAction()
+        {
+            this.FhemResponse = m_fhemClient.SendCommand( this.FhemCommand );
+        }
+
+        //-- Methods
+        #endregion
+        //---------------------------------------------------------------------
+    }
 }
+//-----------------------------------------------------------------------------
